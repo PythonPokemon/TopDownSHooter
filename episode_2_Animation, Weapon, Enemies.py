061b04ef0d77f313a44e3631635330f5d01d99f0
PythonPokemon <Jakob.D.Baron@Outlook.de>
2023-08-22 15:24:31 +0200

    animation update

--- episode_2_Animation, Weapon, Enemies.py
+++ episode_2_Animation, Weapon, Enemies.py
@@ -1,6 +1,7 @@
 import pygame #---importiert pygame bibliothek
 import sys#---?
-import math
+import math #---importiert aus der Python Bibliothek berechnungsgrundlagen
+import random #---mportiert aus der Python Bibliothek zufallsgenerator
 
 pygame.init()#---initialisiert das spiel
 
@@ -17,14 +18,23 @@ class Player:#----objekt Player Parameter
         self.width = width
         self.height = height
         self.animation_count = 0 #--- zählt das animationsframe 0.png - 3.png also maximale anzhal
+        self.moving_right = False
+        self.moving_left = False
     def main(self, display): #---Player auf dem bilschirm zeichnen mit entsprechenden Parametern
         if self.animation_count + 1 >= 16:
             self.animation_count = 0 #---wenn 0.png >= 16 erreicht ist, wird es zurückgesetzt auf 0
             
         self.animation_count += 1#--animationszähler
 
-        display.blit(player_walk_images[self.animation_count//4], (self.x, self.y))
+        if self.moving_right:
+            display.blit(pygame.transform.scale(player_walk_images[self.animation_count//4], (32, 42)), (self.x, self.y))
+        elif self.moving_left:
+             display.blit(pygame.transform.scale(pygame.transform.flip(player_walk_images[self.animation_count//4], True, False), (32, 42)), (self.x, self.y))
+        else:
+            display.blit(pygame.transform.scale(player_walk_images[0], (32, 42)), (self.x, self.y))
         #pygame.draw.rect(display, (255, 0, 0), (self.x, self.y, self.width, self.height))#----farbe
+        self.moving_right = False
+        self.moving_left = False
 
 class PlayerBullet: #----objekt Kugeln Parameter
     def __init__(self, x, y, mouse_x, mouse_y,):
@@ -42,7 +52,43 @@ class PlayerBullet: #----objekt Kugeln Parameter
 
         pygame.draw.circle(display, (0,0,0), (self.x, self.y), 5)
 
-        
+class SlimeEnemy: #----Objekt Slime Parameter
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+        self.animation_images = [pygame.image.load("slime_animation_0.png"), pygame.image.load("slime_animation_1.png"), pygame.image.load("slime_animation_2.png"), pygame.image.load("slime_animation_3.png")]
+        self.animation_count = 0
+        self.reset_offset = 0
+        self.offset_x = random.randrange (-150, 150)
+        self.offset_y = random.randrange (-150, 150)
+    def main(self, display):
+        if self.animation_count + 1 == 16:
+            self.animation_count = 0
+        self.animation_count += 1
+
+        if self.reset_offset == 0:
+            self.offset_x = random.randrange (-150, 150)
+            self.offset_y = random.randrange (-150, 150)
+            self.reset_offset = random.randrange (120, 150)
+        else:
+            self.reset_offset -= 1
+
+        if player.x + self.offset_x > self.x-display_scroll[0]:
+            self.x += 1
+        elif player.x + self.offset_x < self.x-display_scroll[0]:
+            self.x -= 1
+
+        if player.y + self.offset_y > self.y-display_scroll[1]:
+            self.y += 1
+        elif player.y + self.offset_y < self.y-display_scroll[1]:
+            self.y -= 1
+
+
+        display.blit(pygame.transform.scale(self.animation_images[self.animation_count//4], (32, 30)), (self.x-display_scroll[0], self.y-display_scroll[1]))
+
+
+
+enemies = [SlimeEnemy(400, 300)]
 player = Player(400, 300, 32, 32)#-------positionierung auf dem displya x, y, -achse , höhe, breite
 
 display_scroll =[0,0]
@@ -69,11 +115,16 @@ while True:#----schleife
 
     if keys[pygame.K_a]: #---taste a scrollt -5 speed, links
         display_scroll[0] -= 5
+
+        player.moving_left = True
+
         for bullet in player_bullets:
             bullet.x += 5
-
     if keys[pygame.K_d]: #---taste d scrollt -5 speed, rechts
         display_scroll[0] += 5
+
+        player.moving_right = True
+
         for bullet in player_bullets:
             bullet.x -= 5
 
@@ -92,6 +143,9 @@ while True:#----schleife
 
     for bullet in player_bullets:
         bullet.main(display)
+
+    for enemy in enemies:
+        enemy.main(display)
 
     clock.tick(60)
     pygame.display.update()#---schleifen update
